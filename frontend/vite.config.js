42621a739cc0e13cdc3e24f9605328f0b17aa783
FAYAZRIYAZ <167746053+FAYAZRIYAZ@users.noreply.github.com>
2026-09-21 15:42:43 +0530

    Update vite.config.js

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,7 +1,1 @@
-import react from '@vitejs/plugin-react'
-import { defineConfig } from 'vite'
-
-// https://vite.dev/config/
-export default defineConfig({
-  plugins: [react()],
-})
+import react from '@vitejs/plugin-react'; import { defineConfig } from 'vite'; export default defineConfig({ plugins: [react()], base: process.env.GITHUB_ACTIONS ? '/medwin-healthcare/' : '/' });
